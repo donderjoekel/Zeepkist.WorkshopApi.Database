2e4f5e82db5936ecba04cf12cd89e248730d63de
Christiaan Bloemendaal <github@tnrd.net>
2024-03-03 13:51:17 +0100

    feat: initial version

---
+++ Zeepkist.WorkshopApi.Database/Models/Level.cs
@@ -0,0 +1,61 @@
+﻿using JsonApiDotNetCore.Resources;
+using JsonApiDotNetCore.Resources.Annotations;
+
+namespace TNRD.Zeepkist.WorkshopApi.Database.Models;
+
+public partial class Level : IIdentifiable, IIdentifiable<int>
+{
+    [Attr] public int Id { get; set; }
+
+    [Attr] public string Name { get; set; } = null!;
+
+    [Attr] public string ImageUrl { get; set; } = null!;
+
+    [Attr] public DateTime CreatedAt { get; set; }
+
+    [Attr] public DateTime UpdatedAt { get; set; }
+
+    [Attr] public decimal WorkshopId { get; set; }
+
+    [Attr] public bool Valid { get; set; }
+
+    [Attr] public float Validation { get; set; }
+
+    [Attr] public float Gold { get; set; }
+
+    [Attr] public float Silver { get; set; }
+
+    [Attr] public float Bronze { get; set; }
+
+    [Attr] public decimal AuthorId { get; set; }
+
+    [Attr] public string FileHash { get; set; } = null!;
+
+    [Attr] public string FileUrl { get; set; } = null!;
+
+    [Attr] public string FileAuthor { get; set; } = null!;
+
+    [Attr] public string FileUid { get; set; } = null!;
+
+    [Attr] public int? ReplacedBy { get; set; }
+
+    [Attr] public bool Deleted { get; set; }
+
+    string? IIdentifiable.StringId
+    {
+        get => Id.ToString();
+        set
+        {
+            // Left empty on purpose
+        }
+    }
+
+    string? IIdentifiable.LocalId
+    {
+        get => Id.ToString();
+        set
+        {
+            // Left empty on purpose
+        }
+    }
+}
---
+++ Zeepkist.WorkshopApi.Database/ZworpshopContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TNRD.Zeepkist.WorkshopApi.Database.Models;
+
+namespace TNRD.Zeepkist.WorkshopApi.Database;
+
+public partial class ZworpshopContext : DbContext
+{
+    public ZworpshopContext()
+    {
+    }
+
+    public ZworpshopContext(DbContextOptions<ZworpshopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Level> Levels { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Level>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("levels_pkey");
+
+            entity.ToTable("levels");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.AuthorId).HasColumnName("author_id");
+            entity.Property(e => e.Bronze).HasColumnName("bronze");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_at");
+            entity.Property(e => e.Deleted).HasColumnName("deleted");
+            entity.Property(e => e.FileAuthor).HasColumnName("file_author");
+            entity.Property(e => e.FileHash).HasColumnName("file_hash");
+            entity.Property(e => e.FileUid).HasColumnName("file_uid");
+            entity.Property(e => e.FileUrl).HasColumnName("file_url");
+            entity.Property(e => e.Gold).HasColumnName("gold");
+            entity.Property(e => e.ImageUrl).HasColumnName("image_url");
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.ReplacedBy).HasColumnName("replaced_by");
+            entity.Property(e => e.Silver).HasColumnName("silver");
+            entity.Property(e => e.UpdatedAt).HasColumnName("updated_at");
+            entity.Property(e => e.Valid).HasColumnName("valid");
+            entity.Property(e => e.Validation).HasColumnName("validation");
+            entity.Property(e => e.WorkshopId).HasColumnName("workshop_id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
